69d4d7508360901c99fbb4faa19cf10f8e7b124e
Erdembileg <97537483+Zwique@users.noreply.github.com>
2023-12-30 15:01:16 +0800

    Update basic-mod2.py

--- basic-mod2.py
+++ basic-mod2.py
@@ -4,8 +4,8 @@ import string
 with open("message.txt") as m:
     o = m.read()
     
-    letters = string.ascii_uppercase
-    digits = string.digits
+    letters = string.ascii_uppercase        # Importing from A to Z but only in uppercase
+    digits = string.digits                   # Importing from 0 to 9
     down = "_"
     
     flag = ""
@@ -13,15 +13,15 @@ with open("message.txt") as m:
     out = [int (val) for val in o.split()]
     for i in out:
         
-        target = pow(i,-1,41)
+        target = pow(i,-1,41)    # Taking the modular inverse with 41.
         
-        if (target >= 1 and target <= 26):
+        if (target >= 1 and target <= 26):    # Sorting - alphabets
             flag += letters[target - 1]
         
-        elif (target >= 27 and target <= 36):
+        elif (target >= 27 and target <= 36):   # Sorting - decimal numebers
             flag += digits[target - 27]
-        elif (target == 37):
+        elif (target == 37):                    # Sorting - an underscore
             flag += down
-    print(flag)
+    print(flag)                                # The output will look like `1NV3R53LY_H4RD_DADAACAA`, which is the decrypted_message.
 
-    # picoCTF{1NV3R53LY_H4RD_DADAACAA}
+    # picoCTF{1NV3R53LY_H4RD_DADAACAA}          # picoCTF flag format (i.e. picoCTF{decrypted_message})
